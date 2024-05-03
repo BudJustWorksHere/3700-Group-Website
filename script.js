231731d38d18aeaf7d87c5b6aa1d09faed840860
Jadenw9013 <130799431+Jadenw9013@users.noreply.github.com>
2024-05-02 17:26:49 -0700

    Update script.js

--- script.js
+++ script.js
@@ -2,10 +2,8 @@
 document.addEventListener('DOMContentLoaded', function() {
     // Get the button element by its ID
     const articleButton = document.getElementById('articleButton');
-
-    // Add a click event listener to the button
     articleButton.addEventListener('click', function() {
-        // Redirect to the article page when the button is clicked
-        window.location.href = 'article.html'; // Replace 'article.html' with the actual URL of your article page
+        
+        window.location.href = 'article_1.html'; 
     });
 });

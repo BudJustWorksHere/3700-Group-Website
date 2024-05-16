619b71acf9501ac999a2c21eb316e1d14aa4d9f3
Alvaro <alvaro.pena.e@gmail.com>
2024-05-16 16:14:52 -0700

    deleted favicon insert js and added the favicon link to all pages

--- javascript/faviconInsert.js
+++
@@ -1,9 +0,0 @@
-function includeFavicon() {
-    var link = document.createElement('link');
-    link.rel = 'icon';
-    link.type = 'image/x-icon';
-    link.href = 'http://css1.seattleu.edu/~apena1/3700-Website-Group-8/assets/favicon.ico';
-    document.head.appendChild(link);
-}
-
-includeFavicon();
